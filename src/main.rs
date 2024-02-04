a9331ea186ea2c964b072f4e46de2a948b1247a1
cutsea110 <cutsea110@gmail.com>
2024-02-04 15:44:09 +0900

    refine functions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,53 @@
-use postgres::{Client, NoTls};
+use postgres::{Client, NoTls, Transaction};
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct Person {
+    name: String,
+    age: i32,
+    data: Option<Vec<u8>>,
+}
+impl Person {
+    fn new(name: &str, age: i32, data: Option<&[u8]>) -> Self {
+        Self {
+            name: name.to_string(),
+            age,
+            data: data.map(|d| d.to_vec()),
+        }
+    }
+}
+
+fn insert_person(tx: &mut Transaction<'_>, person: &Person) -> i32 {
+    let row = tx
+        .query_one(
+            "INSERT INTO person (name, age, data) VALUES ($1, $2, $3) RETURNING id",
+            &[&person.name, &person.age, &person.data],
+        )
+        .unwrap();
+
+    row.get(0)
+}
+
+fn fetch_person(tx: &mut Transaction<'_>, id: i32) -> Option<Person> {
+    match tx.query_one("SELECT name, age, data FROM person WHERE id = $1", &[&id]) {
+        Ok(row) => Some(Person::new(row.get(0), row.get(1), row.get(2))),
+        Err(e) => {
+            eprintln!("error fetching person: {}", e);
+            None
+        }
+    }
+}
+
+fn my_transaction(client: &mut Client) -> Option<Person> {
+    let mut tx = client.transaction().unwrap();
+
+    let person = Person::new("Ferris", 42, None);
+    let id = insert_person(&mut tx, &person);
+    let person = fetch_person(&mut tx, id);
+
+    tx.commit().unwrap();
+
+    person
+}
 
 fn main() {
     let mut client = Client::connect(
@@ -7,29 +56,10 @@ fn main() {
     )
     .unwrap();
 
-    let mut transaction = client.transaction().unwrap();
+    let person = my_transaction(&mut client);
 
-    let name = "Ferris";
-    let age = 42i32;
-    let data = None::<&[u8]>;
-    transaction
-        .execute(
-            "INSERT INTO person (name, age, data) VALUES ($1, $2, $3)",
-            &[&name, &age, &data],
-        )
-        .unwrap();
-
-    for row in transaction
-        .query("SELECT id, name, age, data FROM person", &[])
-        .unwrap()
-    {
-        let id: i32 = row.get(0);
-        let name: &str = row.get(1);
-        let age: i32 = row.get(2);
-        let data: Option<&[u8]> = row.get(3);
-        println!("found person {} {}({}) {:?}", id, name, age, data);
+    match person {
+        Some(person) => println!("found person {:?}", person),
+        None => println!("no person found"),
     }
-
-    transaction.commit().unwrap();
-    // transaction.rollback().unwrap();
 }
