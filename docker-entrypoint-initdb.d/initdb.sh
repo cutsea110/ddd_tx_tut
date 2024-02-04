9ee0e75fe803865f4de31ca667b28e18c650db68
cutsea110 <cutsea110@gmail.com>
2024-02-04 14:42:36 +0900

    add i32 field to sample

--- docker-entrypoint-initdb.d/initdb.sh
+++ docker-entrypoint-initdb.d/initdb.sh
@@ -3,6 +3,7 @@ psql -U admin sampledb <<EOSQL
 CREATE TABLE person (
   id    SERIAL PRIMARY KEY,
   name  TEXT NOT NULL,
+  age   INTEGER NOT NULL,
   data  BYTEA
 );
 EOSQL
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,29 @@ use postgres::{Client, NoTls};
 
 fn main() {
     let mut client = Client::connect(
-        "host=localhost port=15432 dbname=sampledb user=admin password=admin",
+        "postgresql://admin:adminpass@localhost:15432/sampledb",
         NoTls,
     )
     .unwrap();
 
     let name = "Ferris";
+    let age = 42i32;
     let data = None::<&[u8]>;
     client
         .execute(
-            "INSERT INTO person (name, data) VALUES ($1, $2)",
-            &[&name, &data],
+            "INSERT INTO person (name, age, data) VALUES ($1, $2, $3)",
+            &[&name, &age, &data],
         )
         .unwrap();
 
     for row in client
-        .query("SELECT id, name, data FROM person", &[])
+        .query("SELECT id, name, age, data FROM person", &[])
         .unwrap()
     {
         let id: i32 = row.get(0);
         let name: &str = row.get(1);
-        let data: Option<&[u8]> = row.get(2);
-        println!("found person {} {} {:?}", id, name, data);
+        let age: i32 = row.get(2);
+        let data: Option<&[u8]> = row.get(3);
+        println!("found person {} {}({}) {:?}", id, name, age, data);
     }
 }
