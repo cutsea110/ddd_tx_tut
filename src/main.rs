bac763214e8e6230af1e4734b25b1cf4129da108
cutsea110 <cutsea110@gmail.com>
2024-02-05 17:49:59 +0900

    refine

--- src/main.rs
+++ src/main.rs
@@ -552,6 +552,7 @@ pub mod tx {
         }
     }
 }
+use crate::tx::Tx;
 
 type PersonId = i32;
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -566,67 +567,6 @@ impl Person {
             name: name.to_string(),
             age,
             data: data.map(|d| d.to_vec()),
-        }
-    }
-}
-
-trait PersonRepository {
-    type Tx;
-
-    fn insert_person(tx: &mut Self::Tx, person: &Person) -> PersonId;
-    fn fetch_person(tx: &mut Self::Tx, id: PersonId) -> Option<Person>;
-}
-
-struct PersonRepositoryImpl<'a> {
-    conn_str: &'a str,
-    client: Client,
-}
-impl<'a> PersonRepositoryImpl<'a> {
-    fn new(conn_str: &'a str) -> Self {
-        let client = Client::connect(conn_str, NoTls).unwrap();
-        Self { conn_str, client }
-    }
-
-    fn with_tx<F, T, E>(&mut self, q: F) -> Result<T, E>
-    where
-        F: FnOnce(&mut Transaction<'_>) -> Result<T, E>,
-    {
-        let mut tx = self.client.transaction().unwrap();
-
-        match q(&mut tx) {
-            Ok(v) => {
-                tx.commit().unwrap();
-                Ok(v)
-            }
-
-            Err(e) => {
-                tx.rollback().unwrap();
-                Err(e)
-            }
-        }
-    }
-}
-impl<'a> PersonRepository for PersonRepositoryImpl<'a> {
-    type Tx = Transaction<'a>;
-
-    fn insert_person(tx: &mut Self::Tx, person: &Person) -> PersonId {
-        let row = tx
-            .query_one(
-                "INSERT INTO person (name, age, data) VALUES ($1, $2, $3) RETURNING id",
-                &[&person.name, &person.age, &person.data],
-            )
-            .unwrap();
-
-        row.get(0)
-    }
-
-    fn fetch_person(tx: &mut Self::Tx, id: PersonId) -> Option<Person> {
-        match tx.query_one("SELECT name, age, data FROM person WHERE id = $1", &[&id]) {
-            Ok(row) => Some(Person::new(row.get(0), row.get(1), row.get(2))),
-            Err(e) => {
-                eprintln!("error fetching person: {}", e);
-                None
-            }
         }
     }
 }
@@ -653,22 +593,37 @@ fn fetch_person(tx: &mut Transaction<'_>, id: PersonId) -> Option<Person> {
     }
 }
 
-fn with_tx<F, T, E>(client: &mut Client, q: F) -> Result<T, E>
+// usually, you would use this transaction
+fn pg_run_tx<F, T, E>(client: &mut Client, f: F) -> Result<T, E>
 where
     F: FnOnce(&mut Transaction<'_>) -> Result<T, E>,
 {
-    let mut tx = client.transaction().unwrap();
+    let mut ctx = client.transaction().expect("begin transaction");
 
-    match q(&mut tx) {
-        Ok(ret) => {
-            tx.commit().unwrap();
-            Ok(ret)
+    match tx::with_tx(f).run(&mut ctx) {
+        Ok(result) => {
+            ctx.commit().expect("commit transaction");
+            Ok(result)
         }
         Err(e) => {
-            tx.rollback().unwrap();
+            ctx.rollback().expect("rollback transaction");
             Err(e)
         }
     }
+}
+
+// always rollback at the case of test
+fn pg_run_tx_test<F, T, E>(client: &mut Client, f: F) -> Result<T, E>
+where
+    F: FnOnce(&mut Transaction<'_>) -> Result<T, E>,
+{
+    let mut ctx = client.transaction().expect("begin transaction");
+
+    let result = tx::with_tx(f).run(&mut ctx);
+
+    ctx.rollback().expect("rollback transaction");
+
+    result
 }
 
 fn main() {
@@ -676,16 +631,13 @@ fn main() {
         "postgresql://admin:adminpass@localhost:15432/sampledb",
         NoTls,
     )
-    .unwrap();
+    .expect("create connection");
 
-    let person = with_tx(&mut client, |tx| {
-        let person = Person::new("Ferris", 42, None);
+    let result = pg_run_tx_test(&mut client, |tx| {
+        let person = Person::new("Gauss", 21, None);
         let id = insert_person(tx, &person);
         fetch_person(tx, id).ok_or(())
     });
 
-    match person {
-        Ok(p) => println!("found person {:?}", p),
-        Err(e) => println!("no person found: {:?}", e),
-    }
+    println!("{:?}", result);
 }
