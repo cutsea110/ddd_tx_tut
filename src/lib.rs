4d4c6199b93f8394452053a57b251346ec8a3a23
cutsea110 <cutsea110@gmail.com>
2024-02-07 09:46:43 +0900

    Revert "the new fn which will be usecase's run_tx"
    This reverts commit f741497e2283a4883cbd85ad5d4b7b1188ec676e.
    
    Revert "refactor"
    
    This reverts commit 60fc6a210f07a132d0bf7402745fa241838a454d.
    
    Revert "usecase like code"
    
    This reverts commit 01bd061afa8183ca86b375dcdd4b34814510d5fa.
    
    Revert "refine error value"
    
    This reverts commit da5fe93f1a4ccca8fb0c22063d7d58f6d8fe0e3f.
    
    Revert "arrange module"
    
    This reverts commit b2351c81cc5ab1abd223cb5b861a2981ce274260.
    
    Revert "refine"
    
    This reverts commit 907851e231a990607896126febbc70893a4f04e3.
    
    Revert "remove person usecase"
    
    This reverts commit 76bb0528f7cf0b4763682724361a02327cb0a96f.
    
    Revert "person"
    
    This reverts commit 946b5cd7b99d548d91db464d685c8d4fda11c422.
    
    Revert "rename methods"
    
    This reverts commit efd30a17e6f45a619affde82c7608d8199ea9974.
    
    Revert "refactor"
    
    This reverts commit 866be8165cbcce5cea31ac912a471bffb9684222.
    
    Revert "start to imple usecase"
    
    This reverts commit 6c81b8f889b321374c48951fd6f0edb8c722d79f.
    
    Revert "refactor"
    
    This reverts commit 327c9ed6aca1e727f61daf8c00831a85c4417fd5.
    
    Revert "show data"
    
    This reverts commit c6698644001a5fcc6d1100b14386ae8abf5a865c.
    
    Revert "test more api"
    
    This reverts commit dcfced32c62bc5531718b5ffd99acf48d006191f.
    
    Revert "make person repository crud"
    
    This reverts commit 2cb462f79005d632f09ade37f97075729f22c809.
    
    Revert "arrange pg_db"
    
    This reverts commit 2564c340c1406a5dc318c6f2c1e63bcfabea26d8.
    
    Revert "move pg_db to other file"
    
    This reverts commit 8e6b1ef1fba3fc97ac58980d02ca3f53a1dc578c.
    
    Revert "move tx_rs to lib"
    
    This reverts commit cf0dfd8910c41aa5f726ee9949eb0b38b678e2a9.
    
    Revert "start to arrange project"
    
    This reverts commit 7c8ee8d9caaf764bb40f3ac8350c7602de35547f.
    
    Revert "around Error"
    
    This reverts commit 4c5c488114174d34df3a314c7b09f05b2e7d9ba8.
    
    Revert "fmt::Display for PgPersonRepository"
    
    This reverts commit a239e8aa7f12dfe56412ed641fa8578a798d5182.
    
    Revert "remove unnecessary import"
    
    This reverts commit ee16b15f7e829cdf1475d87fa39c99b37348fe0d.
    
    Revert "construct pg_db as sample"
    
    This reverts commit a7eea8ebd4aae36b9345759c5c1bb809039e7f90.
    
    Revert "imple PgPersonRepository"
    
    This reverts commit 748c83ddcfbdc7aa82b1723624caf028654a3ffa.
    
    Revert "refine"
    
    This reverts commit bac763214e8e6230af1e4734b25b1cf4129da108.
    
    Revert "module tx"
    
    This reverts commit 1ad467275ec085be847c65391995d812c5a2d2df.
    
    Revert "retry to imple person repo"
    
    This reverts commit 35159d2f11fe07e763b09bdaf1da4906763d08a7.
    
    Revert "try to imple person repository"
    
    This reverts commit db0dbb806c27b6b18d6a3e7a0fcebe0bc98e974f.
    
    Revert "type alias PersonId"
    
    This reverts commit e56e652e1e9cc24f87ca8dc3276b0e7b85d6a1a4.
    
    Revert "add comment"
    
    This reverts commit e63a3ba79b1a177179fc499d6c3da6a279d3040a.
    
    Revert "more more refine"
    
    This reverts commit 221cfe8817e6a7e173185d3e1af0b096168c18e0.
    
    Revert "more refine"
    
    This reverts commit 6991f03f27f9db042d2dcb3a593ba21be438bdbe.
    
    Revert "refine functions"
    
    This reverts commit a9331ea186ea2c964b072f4e46de2a948b1247a1.

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-mod tx;
-
-pub use tx::*;
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use postgres::{Client, NoTls};
+
+fn main() {
+    let mut client = Client::connect(
+        "postgresql://admin:adminpass@localhost:15432/sampledb",
+        NoTls,
+    )
+    .unwrap();
+
+    let mut transaction = client.transaction().unwrap();
+
+    let name = "Ferris";
+    let age = 42i32;
+    let data = None::<&[u8]>;
+    transaction
+        .execute(
+            "INSERT INTO person (name, age, data) VALUES ($1, $2, $3)",
+            &[&name, &age, &data],
+        )
+        .unwrap();
+
+    for row in transaction
+        .query("SELECT id, name, age, data FROM person", &[])
+        .unwrap()
+    {
+        let id: i32 = row.get(0);
+        let name: &str = row.get(1);
+        let age: i32 = row.get(2);
+        let data: Option<&[u8]> = row.get(3);
+        println!("found person {} {}({}) {:?}", id, name, age, data);
+    }
+
+    transaction.commit().unwrap();
+    // transaction.rollback().unwrap();
+}
