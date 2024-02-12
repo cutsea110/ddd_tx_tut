5a2946a067050a825d80d1b48244eb12c098a8eb
cutsea110 <cutsea110@gmail.com>
2024-02-12 18:07:01 +0900

    spy tests for service

--- app/service.rs
+++ app/service.rs
@@ -251,3 +251,190 @@ mod mock_tests {
         assert_eq!(result, Ok(expected))
     }
 }
+
+#[cfg(test)]
+mod spy_tests {
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    use crate::{
+        dao::{DaoError, PersonDao},
+        HavePersonDao,
+    };
+
+    use super::*;
+
+    struct DummyPersonDao;
+    impl PersonDao<()> for DummyPersonDao {
+        fn insert(&self, _person: Person) -> impl tx_rs::Tx<(), Item = PersonId, Err = DaoError> {
+            tx_rs::with_tx(move |&mut ()| Ok(1))
+        }
+        fn fetch(
+            &self,
+            _id: PersonId,
+        ) -> impl tx_rs::Tx<(), Item = Option<Person>, Err = DaoError> {
+            tx_rs::with_tx(move |&mut ()| Ok(None))
+        }
+        fn select(&self) -> impl tx_rs::Tx<(), Item = Vec<(PersonId, Person)>, Err = DaoError> {
+            tx_rs::with_tx(move |&mut ()| Ok(vec![]))
+        }
+    }
+
+    struct SpyPersonUsecase {
+        dao: DummyPersonDao,
+        entry: RefCell<Vec<Person>>,
+        find: RefCell<Vec<PersonId>>,
+        entry_and_verify: RefCell<Vec<Person>>,
+        collect: RefCell<i32>,
+    }
+    impl HavePersonDao<()> for SpyPersonUsecase {
+        fn get_dao<'b>(&'b self) -> Box<&impl PersonDao<()>> {
+            Box::new(&self.dao)
+        }
+    }
+    impl PersonUsecase<()> for SpyPersonUsecase {
+        fn entry<'a>(
+            &'a mut self,
+            person: Person,
+        ) -> impl tx_rs::Tx<(), Item = PersonId, Err = UsecaseError>
+        where
+            (): 'a,
+        {
+            self.entry.borrow_mut().push(person);
+
+            // 返り値に意味はない
+            tx_rs::with_tx(|&mut ()| Ok(42 as PersonId))
+        }
+        fn find<'a>(
+            &'a mut self,
+            id: PersonId,
+        ) -> impl tx_rs::Tx<(), Item = Option<Person>, Err = UsecaseError>
+        where
+            (): 'a,
+        {
+            self.find.borrow_mut().push(id);
+
+            // 返り値に意味はない
+            tx_rs::with_tx(|&mut ()| Ok(None))
+        }
+        fn entry_and_verify<'a>(
+            &'a mut self,
+            person: Person,
+        ) -> impl tx_rs::Tx<(), Item = (PersonId, Person), Err = UsecaseError>
+        where
+            (): 'a,
+        {
+            self.entry_and_verify.borrow_mut().push(person.clone());
+
+            // 返り値に意味はない
+            tx_rs::with_tx(move |&mut ()| Ok((42, person)))
+        }
+        fn collect<'a>(
+            &'a mut self,
+        ) -> impl tx_rs::Tx<(), Item = Vec<(PersonId, Person)>, Err = UsecaseError>
+        where
+            (): 'a,
+        {
+            *self.collect.borrow_mut() += 1;
+
+            // 返り値に意味はない
+            tx_rs::with_tx(|&mut ()| Ok(vec![]))
+        }
+    }
+
+    struct SpyPersonService {
+        usecase: Rc<RefCell<SpyPersonUsecase>>,
+    }
+    impl PersonService<'_, ()> for SpyPersonService {
+        type U = SpyPersonUsecase;
+
+        fn run_tx<T, F>(&mut self, f: F) -> Result<T, ServiceError>
+        where
+            F: FnOnce(&mut RefMut<'_, Self::U>, &mut ()) -> Result<T, UsecaseError>,
+        {
+            let mut usecase = self.usecase.borrow_mut();
+            f(&mut usecase, &mut ()).map_err(ServiceError::TransactionFailed)
+        }
+    }
+
+    #[test]
+    fn test_register() {
+        let usecase = Rc::new(RefCell::new(SpyPersonUsecase {
+            dao: DummyPersonDao,
+            entry: RefCell::new(vec![]),
+            find: RefCell::new(vec![]),
+            entry_and_verify: RefCell::new(vec![]),
+            collect: RefCell::new(0),
+        }));
+        let mut service = SpyPersonService {
+            usecase: usecase.clone(),
+        };
+
+        let expected = Person::new("Alice", 20, Some("Alice is sender"));
+
+        let _ = service.register("Alice", 20, "Alice is sender");
+
+        // Usecase のメソッドの呼び出し記録の検証
+        assert_eq!(usecase.borrow().entry.borrow().len(), 0);
+        assert_eq!(usecase.borrow().find.borrow().len(), 0);
+        assert_eq!(usecase.borrow().entry_and_verify.borrow().len(), 1);
+        assert_eq!(*usecase.borrow().collect.borrow(), 0);
+
+        // Service の引数が Usecase にそのまま渡されていることを検証
+        assert_eq!(usecase.borrow().entry_and_verify.borrow()[0], expected);
+    }
+
+    #[test]
+    fn test_batch_import() {
+        let usecase = Rc::new(RefCell::new(SpyPersonUsecase {
+            dao: DummyPersonDao,
+            entry: RefCell::new(vec![]),
+            find: RefCell::new(vec![]),
+            entry_and_verify: RefCell::new(vec![]),
+            collect: RefCell::new(0),
+        }));
+        let mut service = SpyPersonService {
+            usecase: usecase.clone(),
+        };
+
+        let persons = vec![
+            Person::new("Alice", 20, Some("Alice is sender")),
+            Person::new("Bob", 25, Some("Bob is receiver")),
+            Person::new("Eve", 10, Some("Eve is interseptor")),
+        ];
+        let expected = persons.clone();
+
+        let _ = service.batch_import(persons);
+
+        // Usecase のメソッドの呼び出し記録の検証
+        assert_eq!(usecase.borrow().entry.borrow().len(), 3);
+        assert_eq!(usecase.borrow().find.borrow().len(), 0);
+        assert_eq!(usecase.borrow().entry_and_verify.borrow().len(), 0);
+        assert_eq!(*usecase.borrow().collect.borrow(), 0);
+
+        // Service の引数が Usecase にそのまま渡されていることを検証
+        assert_eq!(usecase.borrow().entry.borrow().clone(), expected);
+    }
+
+    #[test]
+    fn list_all() {
+        let usecase = Rc::new(RefCell::new(SpyPersonUsecase {
+            dao: DummyPersonDao,
+            entry: RefCell::new(vec![]),
+            find: RefCell::new(vec![]),
+            entry_and_verify: RefCell::new(vec![]),
+            collect: RefCell::new(0),
+        }));
+        let mut service = SpyPersonService {
+            usecase: usecase.clone(),
+        };
+
+        let _ = service.list_all();
+
+        // Usecase のメソッドの呼び出し記録の検証
+        assert_eq!(usecase.borrow().entry.borrow().len(), 0);
+        assert_eq!(usecase.borrow().find.borrow().len(), 0);
+        assert_eq!(usecase.borrow().entry_and_verify.borrow().len(), 0);
+        assert_eq!(*usecase.borrow().collect.borrow(), 1);
+    }
+}
