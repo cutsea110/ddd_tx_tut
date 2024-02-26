7c100671c1f0a3b8ce00c869d0b2d5158c086e22
cutsea110 <cutsea110@gmail.com>
2024-02-27 00:19:33 +0900

    trace log

--- app/main.rs
+++ app/main.rs
@@ -111,11 +111,14 @@ pub trait PersonCachedService<'a, Conn, Ctx>: PersonService<'a, Ctx> {
     ) -> Result<(PersonId, Person), ServiceError> {
         let cao = self.get_cao();
         let mut con = cao.get_conn().expect("get cache connection");
+        trace!("cache connection obtained");
 
         let result = self.register(name, birth_date, death_date, data);
+        trace!("register person to db: {:?}", result);
 
         if let Ok((id, person)) = &result {
             let _: () = (cao.save(*id, person))(&mut con).expect("save cache");
+            trace!("save person to cache: {}", person);
         }
 
         result
@@ -124,17 +127,22 @@ pub trait PersonCachedService<'a, Conn, Ctx>: PersonService<'a, Ctx> {
     fn cached_find(&'a mut self, id: PersonId) -> Result<Option<Person>, ServiceError> {
         let cao = self.get_cao();
         let mut con = cao.get_conn().expect("get cache connection");
+        trace!("cache connection obtained");
 
         // if the person is found in the cache, return it
         if let Some(p) = cao.find(id)(&mut con).expect("find cache") {
+            trace!("cache hit!: {}", id);
             return Ok(Some(p));
         }
+        trace!("cache miss!: {}", id);
 
         let result = self.find(id)?;
+        trace!("find person in db: {:?}", result);
 
         // if the person is found in the db, save it to the cache
         if let Some(person) = &result {
             let _: () = (cao.save(id, person))(&mut con).expect("save cache");
+            trace!("save person to cache: {}", person);
         }
 
         Ok(result)
@@ -146,6 +154,7 @@ pub trait PersonCachedService<'a, Conn, Ctx>: PersonService<'a, Ctx> {
     ) -> Result<Vec<PersonId>, ServiceError> {
         let cao = self.get_cao();
         let mut con = cao.get_conn().expect("get cache connection");
+        trace!("cache connection obtained");
 
         let ids = self.batch_import(persons.clone())?;
 
@@ -153,6 +162,7 @@ pub trait PersonCachedService<'a, Conn, Ctx>: PersonService<'a, Ctx> {
         ids.iter().zip(persons.iter()).for_each(|(id, person)| {
             let _: () = (cao.save(*id, person))(&mut con).expect("save cache");
         });
+        trace!("save persons to cache: {:?}", ids);
 
         Ok(ids)
     }
@@ -160,6 +170,7 @@ pub trait PersonCachedService<'a, Conn, Ctx>: PersonService<'a, Ctx> {
     fn cached_list_all(&'a mut self) -> Result<Vec<(PersonId, Person)>, ServiceError> {
         let cao = self.get_cao();
         let mut con = cao.get_conn().expect("get cache connection");
+        trace!("cache connection obtained");
 
         let result = self.list_all()?;
 
@@ -167,6 +178,7 @@ pub trait PersonCachedService<'a, Conn, Ctx>: PersonService<'a, Ctx> {
         result.iter().for_each(|(id, person)| {
             let _: () = (cao.save(*id, person))(&mut con).expect("save cache");
         });
+        trace!("save all persons to cache");
 
         Ok(result)
     }
@@ -174,10 +186,16 @@ pub trait PersonCachedService<'a, Conn, Ctx>: PersonService<'a, Ctx> {
     fn cached_unregister(&'a mut self, id: PersonId) -> Result<(), ServiceError> {
         let cao = self.get_cao();
         let mut con = cao.get_conn().expect("get cache connection");
+        trace!("cache connection obtained");
+
         // even if delete from db failed below, this cache clear is not a matter.
         let _: () = (cao.discard(id))(&mut con).expect("delete cache");
+        trace!("cache cleared: {}", id);
 
-        self.unregister(id)
+        let result = self.unregister(id);
+        trace!("delete from db: {}", id);
+
+        result
     }
 }
 impl<'a> PersonCachedService<'a, redis::Connection, postgres::Transaction<'a>>
