f7bcc394ef7b249678d9682657c4ba2649132e88
cutsea110 <cutsea110@gmail.com>
2024-03-03 15:38:25 +0900

    impl notify method on Notifier

--- app/notifier.rs
+++ app/notifier.rs
@@ -1,1 +1,11 @@
+use thiserror::Error;
 
+#[derive(Debug, Error, Clone, PartialEq, Eq)]
+pub enum NotifierError {
+    #[error("notifier unavailable: {0}")]
+    Unavailable(String),
+}
+
+pub trait Notifier {
+    fn notify(&self, to: &str, message: &str) -> Result<(), NotifierError>;
+}
--- app/rabbitmq.rs
+++ app/rabbitmq.rs
@@ -1,27 +1,53 @@
-pub use log::error;
-pub use thiserror::Error;
+pub use log::{error, trace};
 
-#[derive(Error, Debug)]
-pub enum RabbitmqError {
-    #[error("failed to open rabbitmq client")]
-    OpenError,
-}
+use crate::notifier;
 
 pub struct Client {
-    addr: String,
+    async_runtime: tokio::runtime::Runtime,
+    conn: lapin::Connection,
 }
 impl Client {
-    pub fn open(addr: &str) -> Result<Self, RabbitmqError> {
+    pub fn open(addr: &str) -> Result<Self, notifier::NotifierError> {
+        let async_runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        let conn = async_runtime.block_on(async {
+            lapin::Connection::connect(addr, lapin::ConnectionProperties::default())
+                .await
+                .map_err(|e| {
+                    error!("failed to connect to rabbitmq: {}", e);
+                    notifier::NotifierError::Unavailable(e.to_string())
+                })
+        })?;
+
         Ok(Self {
-            addr: addr.to_string(),
+            async_runtime,
+            conn,
         })
     }
-    pub async fn connect(&self) -> Result<lapin::Connection, RabbitmqError> {
-        lapin::Connection::connect(&self.addr, lapin::ConnectionProperties::default())
-            .await
-            .map_err(|e| {
-                error!("failed to connect to rabbitmq: {}", e);
-                RabbitmqError::OpenError
-            })
+}
+
+impl notifier::Notifier for Client {
+    // to: queue name
+    // message: message to send
+    fn notify(&self, to: &str, message: &str) -> Result<(), notifier::NotifierError> {
+        self.async_runtime.block_on(async {
+            let chan = self.conn.create_channel().await.map_err(|e| {
+                error!("failed to create channel: {}", e);
+                notifier::NotifierError::Unavailable(e.to_string())
+            })?;
+            let _ = chan.basic_publish(
+                "",
+                to,
+                lapin::options::BasicPublishOptions::default(),
+                message.as_bytes(),
+                lapin::BasicProperties::default(),
+            );
+            trace!("published: {} to {}", message, to);
+
+            Ok(())
+        })
     }
 }
