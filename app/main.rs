246aebe1047b455b6329accf058dfb00116800f6
cutsea110 <cutsea110@gmail.com>
2024-06-06 07:05:04 +0900

    refactor

--- app/main.rs
+++ app/main.rs
@@ -118,77 +118,97 @@ fn main() {
     }
     env_logger::init();
 
-    let cache_uri =
-        env::var("CACHE_URI").unwrap_or("redis://:adminpass@localhost:16379".to_string());
-    let db_uri = env::var("DATABASE_URI").unwrap_or(
-        // connect_timeout is in seconds
-        "postgres://admin:adminpass@localhost:15432/sampledb?connect_timeout=2".to_string(),
-    );
-    let mq_uri = env::var("AMQP_URI").unwrap_or(
-        // connection_timeout is in milliseconds
-        "amqp://admin:adminpass@localhost:5672/%2f?connection_timeout=2000".to_string(),
-    );
-    let mut service = PersonServiceImpl::new(&db_uri, &cache_uri, &mq_uri);
+    // Initialize service
+    let mut service = {
+        let cache_uri =
+            env::var("CACHE_URI").unwrap_or("redis://:adminpass@localhost:16379".to_string());
+        let db_uri = env::var("DATABASE_URI").unwrap_or(
+            // connect_timeout is in seconds
+            "postgres://admin:adminpass@localhost:15432/sampledb?connect_timeout=2".to_string(),
+        );
+        let mq_uri = env::var("AMQP_URI").unwrap_or(
+            // connection_timeout is in milliseconds
+            "amqp://admin:adminpass@localhost:5672/%2f?connection_timeout=2000".to_string(),
+        );
 
-    let (id, person) = service
-        .cached_register("poor man", date(2001, 9, 11), None, "one person")
-        .expect("register one person");
-    println!("id:{} {:?}", id, person);
+        PersonServiceImpl::new(&db_uri, &cache_uri, &mq_uri)
+    };
 
-    if let Some(p) = service.cached_find(id).expect("find person") {
-        println!("cache hit:{:?}", p);
+    // register, find and death, then unregister
+    {
+        let (id, person) = service
+            .cached_register("poor man", date(2001, 9, 11), None, "one person")
+            .expect("register one person");
+        println!("id:{} {:?}", id, person);
 
-        let death_date = date(2011, 3, 11);
-        println!("death {} at:{:?}", id, death_date);
-        service.cached_death(id, death_date).expect("kill person");
+        if let Some(p) = service.cached_find(id).expect("find person") {
+            println!("cache hit:{:?}", p);
 
-        if let Some(p) = service.cached_find(id).expect("find dead person") {
-            println!("dead person: {:?}", p);
+            let death_date = date(2011, 3, 11);
+            println!("death {} at:{:?}", id, death_date);
+            service.cached_death(id, death_date).expect("kill person");
+
+            if let Some(p) = service.cached_find(id).expect("find dead person") {
+                println!("dead person: {:?}", p);
+            }
         }
-    }
-    service.cached_unregister(id).expect("delete person");
-
-    let persons = vec![
-        (
-            "Abel",
-            date(1802, 8, 5)..=date(1829, 4, 6),
-            "Abel's theorem",
-        ),
-        (
-            "Euler",
-            date(1707, 4, 15)..=date(1783, 9, 18),
-            "Euler's identity",
-        ),
-        (
-            "Galois",
-            date(1811, 10, 25)..=date(1832, 5, 31),
-            "Group Theory",
-        ),
-        (
-            "Gauss",
-            date(1777, 4, 30)..=date(1855, 2, 23),
-            "King of Math",
-        ),
-    ]
-    .into_iter()
-    .map(|(name, life, desc)| PersonDto::new(name, *life.start(), Some(*life.end()), Some(desc)))
-    .collect::<Vec<_>>();
-
-    let ids = service
-        .cached_batch_import(persons.clone())
-        .expect("batch import");
-    println!("batch import done");
-
-    let persons = service.cached_list_all().expect("list all");
-    for (id, _) in &persons {
-        if let Some(p) = service.cached_find(*id).expect("find person") {
-            println!("cache hit:{} {:?}", id, p);
-        }
+        service.cached_unregister(id).expect("delete person");
     }
 
-    for id in ids {
-        println!("unregister id:{}", id);
-        service.cached_unregister(id).expect("unregister");
+    // batch import
+    let ids = {
+        let persons = vec![
+            (
+                "Abel",
+                date(1802, 8, 5)..=date(1829, 4, 6),
+                "Abel's theorem",
+            ),
+            (
+                "Euler",
+                date(1707, 4, 15)..=date(1783, 9, 18),
+                "Euler's identity",
+            ),
+            (
+                "Galois",
+                date(1811, 10, 25)..=date(1832, 5, 31),
+                "Group Theory",
+            ),
+            (
+                "Gauss",
+                date(1777, 4, 30)..=date(1855, 2, 23),
+                "King of Math",
+            ),
+        ]
+        .into_iter()
+        .map(|(name, life, desc)| {
+            PersonDto::new(name, *life.start(), Some(*life.end()), Some(desc))
+        })
+        .collect::<Vec<_>>();
+
+        let ids = service
+            .cached_batch_import(persons.clone())
+            .expect("batch import");
+        println!("batch import done");
+
+        ids
+    };
+
+    // list all
+    {
+        let persons = service.cached_list_all().expect("list all");
+        for (id, _) in &persons {
+            if let Some(p) = service.cached_find(*id).expect("find person") {
+                println!("cache hit:{} {:?}", id, p);
+            }
+        }
+    }
+
+    // unregister
+    {
+        for id in ids {
+            println!("unregister id:{}", id);
+            service.cached_unregister(id).expect("unregister");
+        }
     }
 
     println!("done everything!");
